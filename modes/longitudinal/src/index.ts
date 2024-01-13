5ddaedc9a636169ef351c94c16b09f624af56267
olivert1969 <38795765+olivert1969@users.noreply.github.com>
2024-01-13 16:57:55 -0500

    Update index.js
    add Cine, Previous, Next buttons to toolbar

--- modes/longitudinal/src/index.ts
+++ modes/longitudinal/src/index.ts
@@ -106,6 +106,17 @@ function modeFactory({ modeConfiguration }) {
         'MeasurementTools',
         'Zoom',
         'Pan',
+        'Capture',
+        'Layout',
+        'MPR',
+        'Cine',
+        'Previous',
+        'Next',
+        'MoreTools',
+      ]);
+      toolbarService.createButtonSection(MPR_TOOL_GROUP_ID, [
+        'MeasurementTools',
+        'Zoom',
         'TrackballRotate',
         'WindowLevel',
         'Capture',
