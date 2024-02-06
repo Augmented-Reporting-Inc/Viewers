b01f79c428350281b5e8c053967e352e0b0ac839
olivert1969 <38795765+olivert1969@users.noreply.github.com>
2024-02-05 21:18:03 -0500

    updates to getSopClassHandlerModule default stressecho extensions

--- platform/app/public/config/ar.js
+++ platform/app/public/config/ar.js
@@ -11,6 +11,14 @@ window.config = {
       customizationType: 'ohif.cornerstoneOverlay',
       items: [
         {
+          id: 'InstanceNumber',
+          customizationType: 'ohif.overlayItem',
+          label: 'Instance Number:',
+          title: 'Instance Number',
+          condition: ({ instance }) => instance && instance.InstanceNumber,
+          contentF: ({ instance }) => instance.InstanceNumber,
+        },
+        {
           id: 'AcquisitionNumber',
           customizationType: 'ohif.overlayItem',
           label: 'Acquisition Number:',
