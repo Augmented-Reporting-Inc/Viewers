88a81f4e66bef7298becb01f7ca145ea2a0b70a3
olivert1969 <38795765+olivert1969@users.noreply.github.com>
2025-08-17 09:20:27 -0400

    ar.js updates

--- platform/app/public/config/ar.js
+++ platform/app/public/config/ar.js
@@ -1,17 +1,74 @@
 /** @type {AppTypes.Config} */
 
 window.config = {
-  name: 'config/default.js',
+  name: 'config/ar.js',
   routerBasename: null,
-  // whiteLabeling: {},
   extensions: [],
   modes: [],
   showPatientInfo: 'visible',
-  customizationService: {},
-  showStudyList: true,
-  // some windows systems have issues with more than 3 web workers
+  customizationService: [
+    {
+      cornerstoneOverlayTopLeft: {
+        id: 'cornerstoneOverlayTopLeft',
+        customizationType: 'ohif.cornerstoneOverlay',
+        items: [
+          {
+            id: 'Stage',
+            customizationType: 'ohif.overlayItem',
+            title: 'Stage Name',
+            condition: ({ instance }) => instance && instance.StageName,
+            contentF: ({ instance }) => instance.StageName,
+          },
+          {
+            id: 'View',
+            customizationType: 'ohif.overlayItem',
+            title: 'View Name',
+            condition: ({ instance }) => instance && instance.ViewName,
+            contentF: ({ instance }) => instance.ViewName,
+          },
+          {
+            id: 'Timer',
+            customizationType: 'ohif.overlayItem',
+            title: 'Timer Name',
+            label: 'timer =',
+            condition: ({ instance }) => instance && instance.EventElapsedTimes,
+            contentF: ({ instance, formatters: { formatDuration } }) =>
+              formatDuration(instance.EventElapsedTimes),
+          },
+        ],
+      },
+      cornerstoneOverlayTopRight: {
+        id: 'cornerstoneOverlayTopRight',
+        customizationType: 'ohif.cornerstoneOverlay',
+        items: [
+          {
+            id: 'InstanceNumber',
+            customizationType: 'ohif.overlayItem',
+            title: 'Instance Number',
+            condition: ({ instance }) => instance && instance.InstanceNumber,
+            contentF: ({ instance }) => instance.InstanceNumber,
+          },
+          {
+            id: 'AcquisitionTime',
+            customizationType: 'ohif.overlayItem',
+            title: 'Acquisition Time',
+            condition: ({ instance }) => instance && instance.AcquisitionTime,
+            contentF: ({ instance, formatters: { formatTime } }) =>
+              formatTime(instance.AcquisitionTime),
+          },
+          {
+            id: 'HR',
+            customizationType: 'ohif.overlayItem',
+            title: 'Heart Rate',
+            condition: ({ instance }) => instance && instance.HeartRate,
+            contentF: ({ instance }) => instance.HeartRate + ' bpm',
+          },
+        ],
+      },
+    },
+  ],
+  showStudyList: false,
   maxNumberOfWebWorkers: 3,
-  // below flag is for performance reasons, but it might not work for all servers
   showWarningMessageForCrossOrigin: true,
   showCPUFallbackMessage: true,
   showLoadingIndicator: true,
@@ -22,9 +79,7 @@ window.config = {
   maxNumRequests: {
     interaction: 100,
     thumbnail: 75,
-    // Prefetch number is dependent on the http protocol. For http 2 or
-    // above, the number of requests can be go a lot higher.
-    prefetch: 25,
+    prefetch: 1000,
   },
   useNorm16Texture: true,
   useSharedArrayBuffer: 'AUTO',
@@ -34,22 +89,10 @@ window.config = {
   },
   studyPrefetcher: {
     enabled: true,
-    /* Number of displaysets to be prefetched  (default: 2)*/
     displaySetCount: 2,
-    /**
-     * Max number of concurrent prefetch requests (default: 10)
-     * High numbers may impact on the time to load a new dropped series because
-     * the browser will be busy with all prefetching requests. As soon as the
-     * prefetch requests get fulfilled the new ones from the new dropped series
-     * are sent to the server.
-     *
-     * */
     maxNumPrefetchRequests: 10,
-    /* Display sets loading order (closest (deafult), downward or upward) */
     order: 'closest',
   },
-  // filterQueryParam: false,
-  // Defines multi-monitor layouts
   multimonitor: [
     {
       id: 'split',
@@ -111,16 +154,6 @@ window.config = {
     },
   ],
   defaultDataSourceName: 'dicomweb',
-  /* Dynamic config allows user to pass "configUrl" query string this allows to load config without recompiling application. The regex will ensure valid configuration source */
-  // dangerouslyUseDynamicConfig: {
-  //   enabled: true,
-  //   // regex will ensure valid configuration source and default is /.*/ which matches any character. To use this, setup your own regex to choose a specific source of configuration only.
-  //   // Example 1, to allow numbers and letters in an absolute or sub-path only.
-  //   // regex: /(0-9A-Za-z.]+)(\/[0-9A-Za-z.]+)*/
-  //   // Example 2, to restricts to either hosptial.com or othersite.com.
-  //   // regex: /(https:\/\/hospital.com(\/[0-9A-Za-z.]+)*)|(https:\/\/othersite.com(\/[0-9A-Za-z.]+)*)/
-  //   regex: /.*/,
-  // },
   dataSources: [
     {
       namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
@@ -128,174 +161,33 @@ window.config = {
       configuration: {
         friendlyName: 'AWS S3 Static wado server',
         name: 'aws',
-        wadoUriRoot: 'https://ssoback.futurepacs.com/orthanc/wado',
-        qidoRoot: 'https://ssoback.futurepacs.com/orthanc/dicom-web',
-        wadoRoot: 'https://ssoback.futurepacs.com/orthanc/dicom-web',
         qidoSupportsIncludeField: false,
         imageRendering: 'wadors',
-        thumbnailRendering: 'wadors',
+        thumbnailRendering: 'thumbnail',
         enableStudyLazyLoad: true,
         supportsFuzzyMatching: true,
         supportsWildcard: false,
         staticWado: true,
-        singlepart: 'bulkdata,video',
-        // whether the data source should use retrieveBulkData to grab metadata,
-        // and in case of relative path, what would it be relative to, options
-        // are in the series level or study level (some servers like series some study)
-        bulkDataURI: {
-          enabled: true,
-          relativeResolution: 'studies',
-          transform: url => url.replace('/pixeldata.mp4', '/rendered'),
+        singlepart: 'thumbnail',
+        onConfiguration: dicomWebConfig => {
+          const clinicName = new URLSearchParams(window.location.search).get('clinicName');
+          return {
+            ...dicomWebConfig,
+            wadoRoot: `/${clinicName}`,
+            qidoRoot: `/${clinicName}`,
+            wadoUriRoot: `/${clinicName}`,
+          };
         },
-        omitQuotationForMultipartRequest: true,
-      },
-    },
-
-    {
-      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
-      sourceName: 'ohif2',
-      configuration: {
-        friendlyName: 'AWS S3 Static wado secondary server',
-        name: 'aws',
-        wadoUriRoot: 'https://dd14fa38qiwhyfd.cloudfront.net/dicomweb',
-        qidoRoot: 'https://dd14fa38qiwhyfd.cloudfront.net/dicomweb',
-        wadoRoot: 'https://dd14fa38qiwhyfd.cloudfront.net/dicomweb',
-        qidoSupportsIncludeField: false,
-        supportsReject: false,
-        imageRendering: 'wadors',
-        thumbnailRendering: 'wadors',
-        enableStudyLazyLoad: true,
-        supportsFuzzyMatching: false,
-        supportsWildcard: true,
-        staticWado: true,
-        singlepart: 'bulkdata,video',
-        // whether the data source should use retrieveBulkData to grab metadata,
-        // and in case of relative path, what would it be relative to, options
-        // are in the series level or study level (some servers like series some study)
         bulkDataURI: {
           enabled: true,
           relativeResolution: 'studies',
         },
         omitQuotationForMultipartRequest: true,
-      },
-    },
-    {
-      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
-      sourceName: 'ohif3',
-      configuration: {
-        friendlyName: 'AWS S3 Static wado secondary server',
-        name: 'aws',
-        wadoUriRoot: 'https://d3t6nz73ql33tx.cloudfront.net/dicomweb',
-        qidoRoot: 'https://d3t6nz73ql33tx.cloudfront.net/dicomweb',
-        wadoRoot: 'https://d3t6nz73ql33tx.cloudfront.net/dicomweb',
-        qidoSupportsIncludeField: false,
-        supportsReject: false,
-        imageRendering: 'wadors',
-        thumbnailRendering: 'wadors',
-        enableStudyLazyLoad: true,
-        supportsFuzzyMatching: false,
-        supportsWildcard: true,
-        staticWado: true,
-        singlepart: 'bulkdata,video',
-        // whether the data source should use retrieveBulkData to grab metadata,
-        // and in case of relative path, what would it be relative to, options
-        // are in the series level or study level (some servers like series some study)
-        bulkDataURI: {
-          enabled: true,
-          relativeResolution: 'studies',
-        },
-        omitQuotationForMultipartRequest: true,
-      },
-    },
-
-    {
-      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
-      sourceName: 'local5000',
-      configuration: {
-        friendlyName: 'Static WADO Local Data',
-        name: 'DCM4CHEE',
-        qidoRoot: 'http://localhost:5000/dicomweb',
-        wadoRoot: 'http://localhost:5000/dicomweb',
-        qidoSupportsIncludeField: false,
-        supportsReject: true,
-        supportsStow: true,
-        imageRendering: 'wadors',
-        thumbnailRendering: 'wadors',
-        enableStudyLazyLoad: true,
-        supportsFuzzyMatching: false,
-        supportsWildcard: true,
-        staticWado: true,
-        singlepart: 'video',
-        bulkDataURI: {
-          enabled: true,
-          relativeResolution: 'studies',
-        },
-      },
-    },
-    {
-      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
-      sourceName: 'orthanc',
-      configuration: {
-        friendlyName: 'local Orthanc DICOMWeb Server',
-        name: 'DCM4CHEE',
-        wadoUriRoot: 'http://localhost/pacs/dicom-web',
-        qidoRoot: 'http://localhost/pacs/dicom-web',
-        wadoRoot: 'http://localhost/pacs/dicom-web',
-        qidoSupportsIncludeField: true,
-        supportsReject: true,
-        dicomUploadEnabled: true,
-        imageRendering: 'wadors',
-        thumbnailRendering: 'wadors',
-        enableStudyLazyLoad: true,
-        supportsFuzzyMatching: true,
-        supportsWildcard: true,
-        omitQuotationForMultipartRequest: true,
-        bulkDataURI: {
-          enabled: true,
-          // This is an example config that can be used to fix the retrieve URL
-          // where it has the wrong prefix (eg a canned prefix).  It is better to
-          // just use the correct prefix out of the box, but that is sometimes hard
-          // when URLs go through several systems.
-          // Example URLS are:
-          // "BulkDataURI" : "http://localhost/dicom-web/studies/1.2.276.0.7230010.3.1.2.2344313775.14992.1458058363.6979/series/1.2.276.0.7230010.3.1.3.1901948703.36080.1484835349.617/instances/1.2.276.0.7230010.3.1.4.1901948703.36080.1484835349.618/bulk/00420011",
-          // when running on http://localhost:3003 with no server running on localhost.  This can be corrected to:
-          // /orthanc/dicom-web/studies/1.2.276.0.7230010.3.1.2.2344313775.14992.1458058363.6979/series/1.2.276.0.7230010.3.1.3.1901948703.36080.1484835349.617/instances/1.2.276.0.7230010.3.1.4.1901948703.36080.1484835349.618/bulk/00420011
-          // which is a valid relative URL, and will result in using the http://localhost:3003/orthanc/.... path
-          // startsWith: 'http://localhost/',
-          // prefixWith: '/orthanc/',
-        },
-      },
-    },
-
-    {
-      namespace: '@ohif/extension-default.dataSourcesModule.dicomwebproxy',
-      sourceName: 'dicomwebproxy',
-      configuration: {
-        friendlyName: 'dicomweb delegating proxy',
-        name: 'dicomwebproxy',
-      },
-    },
-    {
-      namespace: '@ohif/extension-default.dataSourcesModule.dicomjson',
-      sourceName: 'dicomjson',
-      configuration: {
-        friendlyName: 'dicom json',
-        name: 'json',
-      },
-    },
-    {
-      namespace: '@ohif/extension-default.dataSourcesModule.dicomlocal',
-      sourceName: 'dicomlocal',
-      configuration: {
-        friendlyName: 'dicom local',
       },
     },
   ],
   httpErrorHandler: error => {
-    // This is 429 when rejected from the public idc sandbox too often.
     console.warn(error.status);
-
-    // Could use services manager here to bring up a dialog/modal if needed.
     console.warn('test, navigate to https://ohif.org/');
   },
   whiteLabeling: {
